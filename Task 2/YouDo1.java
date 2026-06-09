c1e09db0fb931f3374102c3bf19334b8ad1aeebb
Sukta030 <suktachandra20@gmail.com>
2026-06-09 11:25:43 +0600

    Update YouDo1.java

--- Task 2/YouDo1.java
+++ Task 2/YouDo1.java
@@ -1,1 +1,14 @@
+class ContestStats {
+    int[] scores = {85, 90, 95};
 
+    void printFirstScore() {
+        System.out.println(scores[0]);
+    }
+}
+
+public class Main1 {
+    public static void main(String[] args) {
+        ContestStats stats = new ContestStats();
+        stats.printFirstScore();
+    }
+}
